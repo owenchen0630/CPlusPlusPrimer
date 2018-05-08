1c8f84220bf0bdce72fca4817f80a6eaa9768760
Gingko-C <cbg0202@gmail.com>
2018-05-08 14:14:29 +0800

    2018.5.8

---
+++ 11/33 单词转换程序/源.cpp
@@ -0,0 +1,69 @@
+#include <sstream>
+#include <utility>
+using std::pair;
+using std::make_pair;
+#include <fstream>
+using std::ifstream;
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+#include <vector>
+using std::vector;
+#include <string>
+using std::string;
+#include <map>
+using std::map;
+using std::multimap;
+#include <set>
+using std::set;
+#include <iterator>
+using std::istream_iterator;
+using std::ostream_iterator;
+
+const string &transform(const string &s, const map<string, string> &m) {
+	//if (m.find(s) != m.end()) return m[s];//如果要使用下标操作，map不能为const
+	auto ret = m.find(s);
+	if (ret != m.end()) return ret->second;
+	else return s;
+}
+
+map<string, string> buildMap(ifstream &map_file) {
+	map<string, string> res;
+	string key;
+	string value;
+	while (map_file >> key && std::getline(map_file, value)) {
+		value = value.substr(1);//跳过key和value之间的空格，getline会连空格一起获取。
+		res.insert({ key,value });
+	}
+	return res;
+}
+
+void word_transform(ifstream &map_file, ifstream &input) {
+	auto transMap = buildMap(map_file);
+	string line;
+	while (std::getline(input, line)) {
+		std::istringstream wordsInLine(line);
+		string word;
+		bool flag = true;
+		while (wordsInLine >> word) {
+			if (flag == true) {
+				flag = false;
+			}
+			else {
+				cout << " ";
+			}
+			cout << transform(word, transMap);
+		}
+		cout << endl;
+	}
+}
+
+int main() {
+	ifstream mapfile("rules.txt");
+	ifstream input("text.txt");
+	word_transform(mapfile, input);
+	system("pause");
+	return 0;
+}
+
