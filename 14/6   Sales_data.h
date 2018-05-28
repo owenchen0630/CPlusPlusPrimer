67c35c73b96f1e29642e5f0dd06b6f54c6ac9b67
Gingko-C <cbg0202@gmail.com>
2018-05-28 16:15:06 +0800

    2018.5.28

---
+++ 14/6   Sales_data.h
@@ -0,0 +1,84 @@
+#ifndef SALES_DATA_H
+#define SALES_DATA_H
+#include <string>
+struct Sales_data {
+	//友元声明
+	friend Sales_data add(const Sales_data&, const Sales_data&);
+	friend std::ostream &print(std::ostream&, const Sales_data&);
+	friend std::istream &read(std::istream&, Sales_data&);
+	friend std::ostream &operator<<(std::ostream&, const Sales_data&);
+	friend std::istream &operator>>(std::istream&, Sales_data&);
+	friend Sales_data operator+(const Sales_data&, const Sales_data&);
+public:
+	//构造函数
+	Sales_data(const std::string &s, unsigned n, double p) :bookNo(s), units_sold(n), revenue(n*p) {}
+	Sales_data() :Sales_data("", 0, 0) {} //默认构造函数
+	explicit Sales_data(std::istream &is) :Sales_data() {}
+	explicit Sales_data(const std::string &s) :Sales_data(s, 0, 0) {}
+	Sales_data& operator+=(const Sales_data&);
+	std::string isbn() const { return bookNo; }
+	Sales_data& combine(const Sales_data&);
+	double avg_price() const;
+private:
+	std::string bookNo;
+	unsigned units_sold = 0;
+	double revenue = 0.0;
+};
+
+//Sales_data 接口的非成员组成部分的声明
+Sales_data add(const Sales_data&, const Sales_data&);
+std::ostream &print(std::ostream&, const Sales_data&);
+std::istream &read(std::istream&, Sales_data&);
+
+inline double Sales_data::avg_price() const {
+	if (units_sold)
+		return revenue / units_sold;
+	else
+		return 0;
+}
+Sales_data& Sales_data::combine(const Sales_data &rhs) {
+	units_sold += rhs.units_sold;
+	revenue += rhs.revenue;
+	return *this;
+}
+Sales_data add(const Sales_data &lhs, const Sales_data &rhs) {
+	Sales_data sum = lhs;
+	return sum.combine(rhs);
+}
+
+//1 IO类属于不能被拷贝的类型，只能通过引用来传递它们 2 print不负责换行，执行输出任务的函数应该尽量减少对于格式的控制
+std::ostream &print(std::ostream &os, const Sales_data &item) {
+	os << item.isbn() << " " << item.units_sold << " " << item.revenue << " " << item.avg_price();
+	return os;
+}
+
+std::istream &read(std::istream &is, Sales_data &item) {
+	double price = 0;
+	is >> item.bookNo >> item.units_sold >> price;
+	item.revenue = item.units_sold*price;
+	return is;
+}
+
+std::ostream &operator<<(std::ostream &os, const Sales_data &item) {
+	os << item.isbn() << " " << item.units_sold << " " << item.revenue << " " << item.avg_price();
+	return os;
+}
+
+std::istream &operator>>(std::istream &is, Sales_data &item) {
+	double price = 0;
+	is >> item.bookNo >> item.units_sold >> price;
+	item.revenue = item.units_sold*price;
+	return is;
+}
+
+Sales_data operator+(const Sales_data &lhs, const Sales_data &rhs) {
+	Sales_data sum = lhs;
+	return sum.combine(rhs);
+}
+
+Sales_data& Sales_data::operator+=(const Sales_data &rhs) {
+	combine(rhs);
+	return *this;
+}
+
+#endif
