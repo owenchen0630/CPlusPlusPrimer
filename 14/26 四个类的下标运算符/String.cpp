7c02ffe1a6af9e506de35fae1daaf25e8a24fff1
Gingko-C <cbg0202@gmail.com>
2018-05-29 19:30:25 +0800

    2018.5.29

---
+++ 14/26 四个类的下标运算符/String.cpp
@@ -0,0 +1,127 @@
+#include "String.h"
+
+allocator<char> String::alloc{};//使用前声明
+
+String::String(const char *c) {
+	auto newdata = alloc_n_copy(c, c + strlen(c));
+	elements = newdata.first;
+	first_free = cap = newdata.second;
+	cout << "run char* construct!" << endl;
+}
+
+String::String(const String &s) {
+	auto newdata = alloc_n_copy(s.begin(), s.end());
+	elements = newdata.first;
+	first_free = cap = newdata.second;
+	cout << "run copy construct!" << endl;
+}
+
+String::String(String &&s) noexcept
+	:elements(s.elements), first_free(s.first_free), cap(s.cap) {
+	s.elements = s.first_free = s.cap = nullptr;
+	cout << "run move construct!" << endl;
+}
+
+String& String::operator=(const String &s) {
+	auto newdata = alloc_n_copy(s.begin(), s.end());
+	free();
+	elements = newdata.first;
+	first_free = cap = newdata.second;
+	cout << "run operator=!" << endl;
+	return *this;
+}
+
+String& String::operator=(String &&s) noexcept {
+	if (this != &s) {
+		free();
+		elements = s.elements;
+		first_free = s.first_free;
+		cap = s.cap;
+		s.elements = s.first_free = s.cap = nullptr;
+	}
+	cout << "run move operator=!" << endl;
+	return *this;
+}
+
+void String::push_back(const char &c) {
+	chk_n_alloc();
+	alloc.construct(first_free++, c);
+}
+
+std::pair<char*, char*> String::alloc_n_copy(const char *b, const char *e) {
+	auto data = alloc.allocate(e - b);
+	return { data,std::uninitialized_copy(b,e,data) };
+}
+
+void String::reallocate() {
+	auto newcapacity = (size()) ? size() * 2 : 1;
+	reallocate_n(newcapacity);
+}
+
+void String::reallocate_n(size_t n) {
+	auto newdata = alloc.allocate(n);
+	auto elem = elements;
+	auto dest = newdata;
+	for (int i = 0;i != size();i++)
+		alloc.construct(dest++, std::move(*elem++));
+	free();
+	elements = newdata;
+	first_free = dest;
+	cap = newdata + n;
+
+}
+
+void String::free() {
+	std::for_each(elements, first_free, [](char &c) {alloc.destroy(&c);});
+	alloc.deallocate(elements, cap - elements);
+}
+
+std::ostream& print(std::ostream &os, String &s) {
+	for (char &c : s) {
+		os << c;
+	}
+	return os;
+}
+
+std::ostream& operator<<(std::ostream &os, const String &s) {
+	for (char &c : s) {
+		os << c;
+	}
+	return os;
+}
+
+bool operator==(const String &lhs, const String &rhs) {
+	if (lhs.size() != rhs.size()) return false;
+	for (auto lb = lhs.begin(), rb = rhs.begin();lb != lhs.end() && rb != rhs.end();lb++, rb++)
+		if (*lb != *rb) return false;
+	return true;
+}
+bool operator!=(const String &lhs, const String &rhs) {
+	return !(lhs == rhs);
+}
+
+bool operator<(const String &lhs, const String &rhs) {
+	for (auto lb = lhs.begin(), rb = rhs.begin();lb != lhs.end() && rb != rhs.end();lb++, rb++) {
+		if (*lb < *rb) return true;
+		if (*lb > *rb) return false;
+	}
+	if (lhs.size() < rhs.size()) return true;
+	else return false;
+}
+
+bool operator<=(const String &lhs, const String &rhs) {
+	for (auto lb = lhs.begin(), rb = rhs.begin();lb != lhs.end() && rb != rhs.end();lb++, rb++) {
+		if (*lb < *rb) return true;
+		if (*lb > *rb) return false;
+	}
+	if (lhs.size() <= rhs.size()) return true;
+	else return false;
+}
+
+bool operator>(const String &lhs, const String &rhs) {
+	return !(lhs <= rhs);
+}
+
+bool operator>=(const String &lhs, const String &rhs) {
+	return !(lhs < rhs);
+}
---
+++ 14/26 四个类的下标运算符/String.h
@@ -0,0 +1,57 @@
+#pragma once
+#ifndef STRING_H
+#define STRING_H
+#include <utility>
+#include <memory>
+using std::allocator;
+#include <algorithm>
+class String;
+#include <iostream>
+using std::cout;
+using std::endl;
+
+class String {
+	friend std::ostream& print(std::ostream&, String&);
+	friend std::ostream& operator<<(std::ostream&, const String&);
+	friend bool operator==(const String&, const String&);
+	friend bool operator!=(const String&, const String&);
+	friend bool operator>(const String&, const String&);
+	friend bool operator>=(const String&, const String&);
+	friend bool operator<(const String&, const String&);
+	friend bool operator<=(const String&, const String&);
+public:
+	String() :elements(nullptr), first_free(nullptr), cap(nullptr) {}
+	String(const char*);
+	String(const String&);
+	String(String&&) noexcept;
+	//ÖØÔØÔËËã·û
+	String& operator=(const String&);
+	String& operator=(String&&) noexcept;
+	char& operator[](const size_t n) { return *(elements + n); }
+	const char& operator[](const size_t n) const { return *(elements + n); }
+
+	~String() { free(); }
+	void push_back(const char&);
+	size_t size() const { return first_free - elements; }
+	size_t capacity() const { return cap - elements; }
+	char *begin() const { return elements; }
+	char *end() const { return first_free; }
+private:
+	static allocator<char> alloc;
+	std::pair<char*, char*> alloc_n_copy(const char*, const char*);
+	void chk_n_alloc() { if (size() == capacity()) reallocate(); }
+	void reallocate();
+	void reallocate_n(size_t);
+	void free();
+	char *elements;
+	char *first_free;
+	char *cap;
+};
+
+std::ostream& print(std::ostream&, String&);
+std::ostream& operator<<(std::ostream&, const String&);
+
+
+
+#endif // !STRING_H
+
