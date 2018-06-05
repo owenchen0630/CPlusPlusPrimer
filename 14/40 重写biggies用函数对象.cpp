035eb7ff5130dc2504ee864ee8f5328fa2fe3753
Gingko-C <cbg0202@gmail.com>
2018-06-05 15:08:26 +0800

    2018.6.5

--- 14/40 重写biggies用函数对象.cpp
+++ 14/40 重写biggies用函数对象.cpp
@@ -1,49 +1,3 @@
-//#include<vector>
-//using std::vector;
-//#include<string>
-//using std::string;
-//#include<memory>
-//using std::shared_ptr;
-//using std::unique_ptr;
-//using std::weak_ptr;
-//#include<fstream>
-//using std::ifstream;
-//#include<iostream>
-//using std::cout;
-//using std::cin;
-//using std::endl;
-//
-//class longAs {
-//public:
-//	bool operator()(const string &s, int n) {
-//		return s.size() == n;
-//	}
-//};
-//class longerThan {
-//public:
-//	bool operator()(const string &s, int n) {
-//		return s.size() >= n;
-//	}
-//};
-//int main() {
-//	ifstream infile("what faith can do");
-//	string s;
-//	longAs la;
-//	longerThan lt;
-//	vector<int> num(11,0);
-//	while (infile >> s) {
-//		for (int i = 1;i <= 9;i++) {
-//			if (la(s, i)) num[i]++;
-//		}
-//		if (lt(s, 9)) num[10]++;
-//	}
-//	for (int i : num) {
-//		cout << i << endl;
-//	}
-//	system("pause");
-//	return 0;
-//}
-
 #include <algorithm>
 #include <numeric>
 #include <iterator>
@@ -81,7 +35,7 @@ void elimDups(vector<string> &words) {
 	}
 	cout << endl;
 	auto end_unique = std::unique(words.begin(), words.end());
-	//此处words的size没变但是只能打印出4项，也就是说重复部分只有第一项能正常打印  ？？？为什么呢
+
 	for (string i : words) {
 		cout << i << " ";
 	}
